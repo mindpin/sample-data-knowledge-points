75417356a85a47e357397ae3ff25a66cb9f5ef31
fushang318 <fushang318@gmail.com>
2014-07-01 13:06:59 +0800

    fix

--- 1_graphml_to_json.rb
+++ 1_graphml_to_json.rb
@@ -2,11 +2,15 @@ require 'fileutils'
 require File.expand_path('../knowledge_xml_parser', __FILE__)
 
 input_file_path = ARGV[0]
+output_file_path = ARGV[1]
 
 if !File.exists?(input_file_path)
   raise "#{input_file_path} is not exists!"
 end
 
-output_file_path = File.expand_path("temp.json", __FILE__)
+if File.exists?(output_file_path)
+  raise "#{output_file_path} is exists!"
+end
+
 KnowledgeXmlParser.new(input_file_path).output(output_file_path)
-p output_file_path
+p "create #{output_file_path} success"
---
+++ 2_json_to_net.rb
@@ -0,0 +1,12 @@
+input_file_path = ARGV[0]
+net_name = ARGV[1]
+net_desc = net_name
+
+net = KnowledgeNetStore::Net.where(:name => net_name).first
+if !net.blank?
+  net.destroy
+end
+
+json = IO.read(File.expand_path(input_file_path, __FILE__))
+KnowledgeNetStore::Net.from_json(net_name, net_desc, json)
+p "import success!!"
