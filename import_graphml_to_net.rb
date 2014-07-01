94f5e96c5f6fa56a04e4cb2e0b773a2047a16bf3
fushang318 <fushang318@gmail.com>
2014-07-01 12:24:26 +0800

    add  import_graphml_to_net.rb

---
+++ import_graphml_to_net.rb
@@ -0,0 +1,24 @@
+require 'fileutils'
+require './knowledge_xml_parser'
+
+input_file_path = ARGV[0]
+
+if !File.exists?(input_file_path)
+  raise "#{input_file_path} is not exists!"
+end
+
+output_file_path = File.expand_path("temp.json", __FILE__)
+KnowledgeXmlParser.new(input_file_path).output(output_file_path)
+
+
+net_name = File.basename(input_file_path,".graphml")
+net_desc = net_name
+
+net = KnowledgeNetStore::Net.where(:name => net_name).first
+if !net.blank?
+  net.destroy
+end
+
+json = IO.read(File.expand_path(output_file_path, __FILE__))
+KnowledgeNetStore::Net.from_json(net_name, net_desc, json)
+p "import success!!"
---
+++ knowledge_xml_parser.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'nokogiri'
+require 'json'
+
+class KnowledgeXmlParser
+
+  def initialize(file_path)
+    str = File.new(file_path).read
+
+    @file_xml = Nokogiri::XML str
+  end
+
+  def nodes
+    @file_xml.css('graphml > graph > node')
+  end
+
+  def edges
+    @file_xml.css('graphml > graph > edge')
+  end
+
+  def output(output_file_path)
+    hash = _build_hash
+    json = JSON.generate(hash)
+    IO.write(output_file_path, json)
+  end
+
+  private
+    def _build_hash
+      points_arr = nodes.map do |node_dom|
+        {
+          :id => node_dom["id"], 
+          :name => node_dom.at_css('y|NodeLabel').children.first,
+          :desc => node_dom.at_css('data[key=d5]').text
+        }
+      end
+
+      edges_arr = edges.map do |link|
+        {
+          :parent => link["source"],
+          :child  => link["target"]
+        }
+      end
+
+      {
+        :points => points_arr,
+        :edges => edges_arr
+      }
+    end
+
+    # 产生一个随机字符串
+    def randstr(length = 6)
+      base = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      size = base.size
+      re = '' << base[rand(size-10)]
+      (length - 1).times {
+        re << base[rand(size)]
+      }
+      re
+    end
+
+end
